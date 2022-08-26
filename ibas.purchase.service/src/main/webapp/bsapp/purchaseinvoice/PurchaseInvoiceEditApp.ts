a7af9616d41c6573774adb90730813165dc6bc0d
牛加人等于朱 <niuren.zhu@icloud.com>
2022-08-26 17:10:59 +0800

    修复价格清单切换税处理

--- ibas.purchase.service/src/main/webapp/bsapp/purchaseinvoice/PurchaseInvoiceEditApp.ts
+++ ibas.purchase.service/src/main/webapp/bsapp/purchaseinvoice/PurchaseInvoiceEditApp.ts
@@ -336,14 +336,27 @@ namespace purchase {
                         onCompleted: (opRslt) => {
                             for (let item of opRslt.resultObjects) {
                                 this.editData.purchaseInvoiceItems.forEach((value) => {
-                                    if (item.itemCode === value.itemCode) {
-                                        // 设置价格，注意折前
+                                    if (item.taxed === ibas.emYesNo.YES) {
+                                        // 含税价格
                                         value.isLoading = true;
-                                        value.unitPrice = 0;
                                         value.discount = 1;
+                                        value.unitPrice = 0;
+                                        value.preTaxPrice = 0;
                                         value.price = 0;
                                         value.isLoading = false;
+                                        // 税后价格
                                         value.price = item.price;
+                                        value.currency = item.currency;
+                                    } else {
+                                        // 不含税价格
+                                        value.isLoading = true;
+                                        value.discount = 1;
+                                        value.unitPrice = 0;
+                                        value.preTaxPrice = 0;
+                                        value.price = 0;
+                                        value.isLoading = false;
+                                        // 税前价格
+                                        value.preTaxPrice = item.price;
                                         value.currency = item.currency;
                                     }
                                 });
@@ -720,6 +733,14 @@ namespace purchase {
             protected turnToPurchaseCreditNote(): void {
                 if (ibas.objects.isNull(this.editData) || this.editData.isDirty === true) {
                     this.messages(ibas.emMessageType.WARNING, ibas.i18n.prop("shell_data_saved_first"));
+                    return;
+                }
+                if ((this.editData.approvalStatus !== ibas.emApprovalStatus.APPROVED && this.editData.approvalStatus !== ibas.emApprovalStatus.UNAFFECTED)
+                    || this.editData.deleted === ibas.emYesNo.YES
+                    || this.editData.canceled === ibas.emYesNo.YES
+                    || this.editData.documentStatus === ibas.emDocumentStatus.PLANNED
+                ) {
+                    this.messages(ibas.emMessageType.ERROR, ibas.i18n.prop("purchase_invaild_status_not_support_turn_to_operation"));
                     return;
                 }
                 let target: bo.PurchaseCreditNote = new bo.PurchaseCreditNote();
