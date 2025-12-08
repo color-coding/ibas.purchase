7347e58f15a10914b18482d278b7a35187d10f9f
Niuren.Zhu <niuren.zhu@icloud.com>
2025-12-08 12:37:06 +0800

    修复开启成本计算取消逻辑问题

--- ibas.purchase/src/main/java/org/colorcoding/ibas/purchase/bo/purchasedelivery/PurchaseDeliveryItem.java
+++ ibas.purchase/src/main/java/org/colorcoding/ibas/purchase/bo/purchasedelivery/PurchaseDeliveryItem.java
@@ -10,6 +10,7 @@ import javax.xml.bind.annotation.XmlType;
 
 import org.colorcoding.ibas.accounting.logic.IJECPropertyValueGetter;
 import org.colorcoding.ibas.accounting.logic.ITaxGroupCheckContract;
+import org.colorcoding.ibas.bobas.approval.IApprovalData;
 import org.colorcoding.ibas.bobas.bo.BusinessObject;
 import org.colorcoding.ibas.bobas.bo.IBOTagCanceled;
 import org.colorcoding.ibas.bobas.bo.IBOTagDeleted;
@@ -22,6 +23,7 @@ import org.colorcoding.ibas.bobas.core.IPropertyInfo;
 import org.colorcoding.ibas.bobas.data.ArrayList;
 import org.colorcoding.ibas.bobas.data.DateTime;
 import org.colorcoding.ibas.bobas.data.Decimal;
+import org.colorcoding.ibas.bobas.data.emApprovalStatus;
 import org.colorcoding.ibas.bobas.data.emBOStatus;
 import org.colorcoding.ibas.bobas.data.emDocumentStatus;
 import org.colorcoding.ibas.bobas.data.emYesNo;
@@ -2694,6 +2696,19 @@ public class PurchaseDeliveryItem extends BusinessObject<PurchaseDeliveryItem> i
 				if (PurchaseDeliveryItem.this.parent instanceof IBOTagDeleted) {
 					IBOTagDeleted boTag = (IBOTagDeleted) PurchaseDeliveryItem.this.parent;
 					if (boTag.getDeleted() == emYesNo.YES) {
+						return true;
+					}
+				}
+				if (PurchaseDeliveryItem.this.parent instanceof IApprovalData) {
+					IApprovalData boAp = (IApprovalData) PurchaseDeliveryItem.this.parent;
+					if (boAp.getApprovalStatus() == emApprovalStatus.PROCESSING) {
+						return true;
+					}
+				}
+				if (PurchaseDeliveryItem.this.parent instanceof IApprovalData) {
+					IApprovalData apData = (IApprovalData) PurchaseDeliveryItem.this.parent;
+					if (apData.getApprovalStatus() != emApprovalStatus.UNAFFECTED
+							&& apData.getApprovalStatus() != emApprovalStatus.APPROVED) {
 						return true;
 					}
 				}
